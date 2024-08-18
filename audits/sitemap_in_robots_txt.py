f8a8c01bc8c9c60ccb473af0f9b91a042c740c35
Alex Ruco <alex@ruco.pt>
2024-08-18 11:09:00 +0100

    if not sitemap is found in robots, but there are sitemaps found in the website, indicate the sitemaps on the issues text

--- audits/sitemap_in_robots_txt.py
+++ audits/sitemap_in_robots_txt.py
@@ -2,7 +2,7 @@
 
 from malcom import AuditBase
 from malcom.metadata.architecture_metadata import ARCHITECTURE_METADATA
-from dourado import sitemap_indicated_on_robots
+from dourado import sitemap_indicated_on_robots, website_sitemaps
 
 class SitemapInRobotsTxtAudit(AuditBase):
     def __init__(self):
@@ -15,10 +15,17 @@ class SitemapInRobotsTxtAudit(AuditBase):
             use_cases=metadata["use_cases"]
         )
 
-    def run(self, website):        
+    def run(self, website):
+        # Check if any sitemap is indicated in robots.txt
         sitemap_indicated = sitemap_indicated_on_robots(website_url=website)
-        if sitemap_indicated:  # Check for 'sitemap' in robots.txt
+        if sitemap_indicated:
             self.passed = True
         else:
+            # If no sitemap is found in robots.txt, check for any sitemaps found on the website
             self.passed = False
-            self.issues.append("No sitemap listed in robots.txt.")
+            sitemaps_found = website_sitemaps(website_url=website)
+            if sitemaps_found:
+                sitemaps_list = ', '.join(sitemaps_found)
+                self.issues.append(f"No sitemap listed in robots.txt, but found these sitemaps on the website: {sitemaps_list}.")
+            else:
+                self.issues.append("No sitemap listed in robots.txt and no sitemaps found on the website.")
